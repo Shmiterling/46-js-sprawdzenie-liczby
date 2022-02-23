dc80fb79953a7b7799a3b8981b3bb7b1849fcebd
dbessarab <d.bessarab@gmail.com>
2022-02-23 12:38:38 +0100

    46.1

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// function myFunction (n) {
+//   for (let i = 100; i = n, i >= 100, i<=200;){
+//     return "Liczba znajduje się w przedziale";
+//   } return "Liczba nie znajduje się w przedziale";
+// }
+// console.log(myFunction(n));
+
+function myFunction1(n) {
+  if (n >= 100 && n <= 200) {
+  return "Liczba znajduje się w przedziale";} else {
+    return "Liczba nie znajduje się w przedziale";
+  }
+}
+ console.log(myFunction1(201));
